46e3a4f445ddc616ec1b48d48e36d074326bfdd0
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-05 23:09:28 +0800

    Added initial view for calculator

---
+++ public/js/calculator.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#binary-output-sign').val('1');
+    $('#binary-output-expo').val('11010010');
+    $('#binary-output-expo').mask('0000 0000');
+    $('#binary-output-frac').val('10001000100010001000100010001000');
+    $('#binary-output-frac').mask('0000 0000 0000 0000 0000 0000 0000 0000');
+});
