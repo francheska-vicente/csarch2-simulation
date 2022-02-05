b4e2a79f9e1ab126f2448bbc6ade24dd0bc122f9
sophia-vista <75743382+sophia-vista@users.noreply.github.com>
2022-02-05 23:29:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+hostname = process.env.HOSTNAME
+port = process.env.PORT
+
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
@@ -13,8 +16,8 @@ hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use('/', routes);
 
-app.listen(process.env.PORT, process.env.HOSTNAME, function () {
+app.listen(port, hostname, function () {
     console.log(
-        `Server is running at http://${process.env.HOSTNAME}:${process.env.PORT}`
+        `Server is running at http://${hostname}:${port}`
     );
 });
