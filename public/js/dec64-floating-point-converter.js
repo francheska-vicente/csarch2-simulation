0a760df35b6ab6f4cb075403d44a17ed90e722c2
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2022-02-07 17:19:34 +0800

    Update dec64-floating-point-converter.js

--- public/js/dec64-floating-point-converter.js
+++ public/js/dec64-floating-point-converter.js
@@ -17,70 +17,72 @@ function decimalToDec64Float(decimal, exponent) {
         typeof decimal == 'number'
             ? normalizeDecimal(String(decimal))
             : normalizeDecimal(decimal);
-    
+
     //TODO: Remove after debugging
-     console.log ("NORMALIZED: " + normalizedDecimal);
+    console.log('NORMALIZED: ' + normalizedDecimal);
 
     // Rounding-off
-    roundedDecimal = String(getRoundedOffNum(decimal, normalizedDecimal, ROUNDOFF_DEF));
+    roundedDecimal = String(
+        getRoundedOffNum(decimal, normalizedDecimal, ROUNDOFF_DEF)
+    );
 
     //TODO: Remove after debugging
-     console.log ("ROUNDEDOFF: " + roundedDecimal);
+    console.log('ROUNDEDOFF: ' + roundedDecimal);
 
-    const normalizedExponent = exponent - calculateFloatDisplacement(String(decimal)); // right: subtract; left: add
+    const normalizedExponent =
+        exponent - calculateFloatDisplacement(String(decimal)); // right: subtract; left: add
     const exponentBias = normalizedExponent + EXPONENT_BIAS;
     console.log(normalizedExponent);
     if (normalizedExponent > 384 || normalizedExponent < -383)
         return getInfinityRepresentation(normalizedExponent);
 
-    const signBit = getSignBit(roundedDecimal)
-    const combinationField = getCombinationField(roundedDecimal, exponentBias)
-    const exponentContinuation = getExponentContinuation(exponentBias)
-    const coefficientContinuation = getCoefficientContinuation(roundedDecimal)
+    const signBit = getSignBit(roundedDecimal);
+    const combinationField = getCombinationField(roundedDecimal, exponentBias);
+    const exponentContinuation = getExponentContinuation(exponentBias);
+    const coefficientContinuation = getCoefficientContinuation(roundedDecimal);
 
-    const binary1 =   (signBit+combinationField+exponentContinuation).toString()+coefficientContinuation.toString().toString().substring(0,2)
-    const binary2 =   coefficientContinuation.toString().substring(2).split(',').join('');
+    const binary1 =
+        (signBit + combinationField + exponentContinuation).toString() +
+        coefficientContinuation.toString().toString().substring(0, 2);
+    const binary2 = coefficientContinuation
+        .toString()
+        .substring(2)
+        .split(',')
+        .join('');
 
-    const hex1 = parseInt(binary1, 2).toString(16).toUpperCase().padStart(4, '0');
-    const hex2 = parseInt(binary2, 2).toString(16).toUpperCase().padStart(12, '0');
+    const hex1 = parseInt(binary1, 2)
+        .toString(16)
+        .toUpperCase()
+        .padStart(4, '0');
+    const hex2 = parseInt(binary2, 2)
+        .toString(16)
+        .toUpperCase()
+        .padStart(12, '0');
 
     const decimal64Format = {
         signBit: signBit,
         combinationField: combinationField,
         exponentContinuation: exponentContinuation,
         coefficientContinuation: coefficientContinuation,
-        hex: hex1+hex2,
+        hex: hex1 + hex2,
     };
-    console.log(decimal64Format)
+    console.log(decimal64Format);
     return decimal64Format;
 }
 
 function getNaNRepresentation() {
     return {
-        signaling: {
-            signBit: 'X',
-            combinationField: '11111',
-            exponentContinuation: '1XXXXXXX',
-            coefficientContinuation: [
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-            ],
-        },
-        quiet: {
-            signBit: 'X',
-            combinationField: '11111',
-            exponentContinuation: 'XXXXXXXX',
-            coefficientContinuation: [
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-                'XXXXXXXXXX',
-            ],
-        },
+        //quiet
+        signBit: 'X',
+        combinationField: '11111',
+        exponentContinuation: 'XXXXXXXX',
+        coefficientContinuation: [
+            'XXXXXXXXXX',
+            'XXXXXXXXXX',
+            'XXXXXXXXXX',
+            'XXXXXXXXXX',
+            'XXXXXXXXXX',
+        ],
     };
 }
 
@@ -100,18 +102,16 @@ function getInfinityRepresentation(exponent) {
         };
     } else
         return {
-            positive: {
-                signBit: '0',
-                combinationField: '11110',
-                exponentContinuation: 'XXXXXXXX',
-                coefficientContinuation: [
-                    'XXXXXXXXXX',
-                    'XXXXXXXXXX',
-                    'XXXXXXXXXX',
-                    'XXXXXXXXXX',
-                    'XXXXXXXXXX',
-                ],
-            },
+            signBit: '0',
+            combinationField: '11110',
+            exponentContinuation: 'XXXXXXXX',
+            coefficientContinuation: [
+                'XXXXXXXXXX',
+                'XXXXXXXXXX',
+                'XXXXXXXXXX',
+                'XXXXXXXXXX',
+                'XXXXXXXXXX',
+            ],
         };
 }
 
@@ -143,9 +143,14 @@ function getCombinationField(decimal, exponent) {
 function normalizeDecimal(decimal) {
     // Remove negative sign
     //let normalized = Math.abs(decimal);   // Math.abs automatically rounds numbers above >16 so it's changed to the code below
-    let normalized = String(decimal).includes('-') ? String(decimal).slice(1, String(decimal).length) : String(decimal);
+    let normalized = String(decimal).includes('-')
+        ? String(decimal).slice(1, String(decimal).length)
+        : String(decimal);
     // Remove radix point and start padding 0s if necessary
-    normalized = String(normalized).replace('.', '').padStart(16, '0').substring(0,16);
+    normalized = String(normalized)
+        .replace('.', '')
+        .padStart(16, '0')
+        .substring(0, 16);
     // Add negative sign back
     normalized = decimal >= 0 ? normalized : `-${normalized}`;
 
@@ -157,7 +162,7 @@ function getPass17thDigit(decimal) {
     let normalized = decimal.charAt(0) == '-' ? decimal.substring(1) : decimal;
     // Remove wholenumber
     normalized = normalized.replace('.', '').padStart(16, '0').substring(16);
-    if (normalized == "") return '0';
+    if (normalized == '') return '0';
 
     return normalized;
 }
@@ -178,36 +183,44 @@ function getCoefficientContinuation(decimal) {
 
 function calculateFloatDisplacement(decimal) {
     let [beforeRadixPoint, afterRadixPoint] = String(decimal).split('.');
-    beforeRadixPoint = beforeRadixPoint.charAt(0) == '-' ? beforeRadixPoint.substring(1) : beforeRadixPoint;
+    beforeRadixPoint =
+        beforeRadixPoint.charAt(0) == '-'
+            ? beforeRadixPoint.substring(1)
+            : beforeRadixPoint;
 
     // Check if decimal only contains 0
-    if (/^0*$/.test(afterRadixPoint))
-        afterRadixPoint = undefined
-    
-    if (afterRadixPoint == undefined){
+    if (/^0*$/.test(afterRadixPoint)) afterRadixPoint = undefined;
+
+    if (afterRadixPoint == undefined) {
         // Check if wholenumber is greater than 16
         if (beforeRadixPoint.length > 16) return beforeRadixPoint.length - 16;
         return 0;
     } else {
         // Check if wholenumber is greater than 16 but has decimal
         //if (beforeRadixPoint.length > 16) return beforeRadixPoint.length - 16 + afterRadixPoint.length;
-        if (beforeRadixPoint.length > 16) return 16 - beforeRadixPoint.length;  // e.g. 123456789123456789.123 = 1234567891234567.89123
-        if (beforeRadixPoint.length == 16) return 0;    // e.g. 1234567891234567.89
-        if (beforeRadixPoint.length < 16 && beforeRadixPoint.length + afterRadixPoint.length >= 16) return 16 - beforeRadixPoint.length;   // e.g. 123.456789123456789123 or 12345.6789123456789
+        if (beforeRadixPoint.length > 16) return 16 - beforeRadixPoint.length; // e.g. 123456789123456789.123 = 1234567891234567.89123
+        if (beforeRadixPoint.length == 16) return 0; // e.g. 1234567891234567.89
+        if (
+            beforeRadixPoint.length < 16 &&
+            beforeRadixPoint.length + afterRadixPoint.length >= 16
+        )
+            return 16 - beforeRadixPoint.length; // e.g. 123.456789123456789123 or 12345.6789123456789
     }
 
     // if (afterRadixPoint == undefined) return 0;
 
-    return afterRadixPoint.length;  // e.g. 123.456
+    return afterRadixPoint.length; // e.g. 123.456
 }
 
-function getRoundedOffNum (decimal, normalized, method) {
+function getRoundedOffNum(decimal, normalized, method) {
     let decTrunc, decCeil, decFloor;
     // Remove negative sign from unnormalized decimal
-    let tempDec = String(decimal).includes('-') ? String(decimal).slice(1, String(decimal).length) : String(decimal);
+    let tempDec = String(decimal).includes('-')
+        ? String(decimal).slice(1, String(decimal).length)
+        : String(decimal);
     // If decimal/fraction has more than 16 digits as a whole (excluding negative sign)
     if (tempDec.length > 17) {
-        fullDec = normalized + "." + getPass17thDigit(decimal);
+        fullDec = normalized + '.' + getPass17thDigit(decimal);
         decTrunc = Math.trunc(parseInt(fullDec));
         decCeil = Number(normalized) + 1;
         decFloor = Math.floor(parseInt(fullDec));
@@ -218,25 +231,31 @@ function getRoundedOffNum (decimal, normalized, method) {
         decCeil = Math.ceil(normalized / 10) * 10;
         decFloor = Math.floor(normalized / 10) * 10;
     }
-    
+
     switch (method) {
-        case 1: return decTrunc;
-        case 2: return decCeil;
-        case 3: return decFloor;
-        case 4: case 5: return getTies(decCeil, decFloor, method, normalized);
+        case 1:
+            return decTrunc;
+        case 2:
+            return decCeil;
+        case 3:
+            return decFloor;
+        case 4:
+        case 5:
+            return getTies(decCeil, decFloor, method, normalized);
     }
 }
 
-function getTies (ceiling, floor, method, decimal) {
-    if (getPass17thDigit(decimal) % 5 == 0) {  // If it is a tie
-        if (method == 4)    // Ties to zero
+function getTies(ceiling, floor, method, decimal) {
+    if (getPass17thDigit(decimal) % 5 == 0) {
+        // If it is a tie
+        if (method == 4)
+            // Ties to zero
             // If negative, round down. Otherwise, round up
             return decimal < 0 ? floor : ceiling;
-        else    // Ties to even (If ceiling is even, round up. Otherwise, round down)
-            return ceiling % 20 == 0 ? ceiling : floor; // temporary value
-    }
-    else    // Round to nearest number
-        return Math.round(decimal);
+        // Ties to even (If ceiling is even, round up. Otherwise, round down)
+        else return ceiling % 20 == 0 ? ceiling : floor; // temporary value
+    } // Round to nearest number
+    else return Math.round(decimal);
 }
 
 // Negative exponent
